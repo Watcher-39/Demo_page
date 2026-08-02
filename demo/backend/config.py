e3a1c46a0725ec1bf682cb0c61e7989fd3d06f70
Watcher-39 <68494856+Watcher-39@users.noreply.github.com>
2026-08-02 13:47:05 -0400

    docker

--- demo/backend/config.py
+++ demo/backend/config.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///demo.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
--- demo/backend/main.py
+++ demo/backend/main.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from config import app, db
 from models import Admin, Product
+import os
 
 @app.route("/admins", methods=["GET"])
 def get_admins():
@@ -109,4 +110,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
